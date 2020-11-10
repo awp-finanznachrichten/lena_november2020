c335b9253a6c1be9df6582cec441b7f068eea9b4
Simon Wolanin <sw@awp.ch>
2020-11-10 17:12:43 +0100

    Update 10.11.

--- lena_run.R
+++ lena_run.R
@@ -223,16 +223,16 @@ for (k in 1:length(kantonal_short) ) {
                                   kantonal_add[k])
   
   #Daten simulieren Gemeinde!!!
-  #for (a in 1:nrow(results)) { 
+  for (a in 1:nrow(results)) { 
     
-  #  results$gebietAusgezaehlt[a] = TRUE
+    results$gebietAusgezaehlt[a] = TRUE
     
-  #  results$jaStimmenAbsolut[a] <- sample(0:10000,1)
-  #  results$neinStimmenAbsolut[a] <- sample(0:10000,1)
-  #  results$gueltigeStimmen[a] <- results$jaStimmenAbsolut[a] + results$neinStimmenAbsolut[a]
-  #  results$jaStimmenInProzent[a] <- results$jaStimmenAbsolut[a]*100/results$gueltigeStimmen[a]
+    results$jaStimmenAbsolut[a] <- sample(0:10000,1)
+    results$neinStimmenAbsolut[a] <- sample(0:10000,1)
+    results$gueltigeStimmen[a] <- results$jaStimmenAbsolut[a] + results$neinStimmenAbsolut[a]
+    results$jaStimmenInProzent[a] <- results$jaStimmenAbsolut[a]*100/results$gueltigeStimmen[a]
     
-  #}
+  }
 
   #Daten anpassen Gemeinden
   results <- treat_gemeinden(results)
